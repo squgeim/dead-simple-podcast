68fd0970454c9d4f9b12b40020b74c8851690863
squgeim <shreyadahal@gmail.com>
2022-10-09 12:00:04 -0400

    feat: base routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Player from '../lib/player/index.svelte';
+</script>
+
+<h1>Dead Simple Podcast Player</h1>
+<nav>
+	<a href="/">Now Playing</a>
+	<a href="/episodes">Episodes</a>
+	<a href="/subscriptions">Subscriptions</a>
+	<a href="/add">Add Podcast</a>
+</nav>
+
+<slot />
+
+<Player />
